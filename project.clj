5f3b3a786d95b84095a54a105bce0d60a22e97cc
Damion Junk <junkda@gmail.com>
2015-05-22 10:47:55 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject damionjunk/common "0.1.0-SNAPSHOT"
+  :description "A collection of common code bits, slowly accumulating as needed."
+  :url "https://github.com/damionjunk/damionjunk.common"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
